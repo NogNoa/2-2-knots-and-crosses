bf63791ed2b0aabf3502d856d0bc12e46c004f67
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-09-17 11:39:18 +0300

    Add files via upload

--- 2^2 knots and crosses.py
+++ 2^2 knots and crosses.py
@@ -4,7 +4,6 @@ Created on Tue Sep 17 11:00:59 2019
 
 @author: omer
 """
-
 def Check_win(L,Size):
     if type(L)!=list:
         print('This isn\'t a list mate')
@@ -12,5 +11,10 @@ def Check_win(L,Size):
     L=L[:Size**2]
     win=False
     for i in range(Size):
+        print('i=',i)
         for j in [1,Size,Size+1]:
+            print('j=',j)
             win= win or L[Size*i]==L[Size*i+j]
+            print('win=',win)
+    return win
+print(Check_win([1,2,0,4],2))
